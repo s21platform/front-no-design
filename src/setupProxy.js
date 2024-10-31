37405674f05f22c1ff57d0b348773f41c705e2a8
asepopov <asepopov@avito.ru>
2024-11-01 00:42:19 +0300

    add chat and notification

---
+++ src/setupProxy.js
@@ -0,0 +1,30 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
+module.exports = function(app) {
+    app.use(
+        '/auth',
+        createProxyMiddleware({
+            target: 'http://217.28.222.68:6050', // Сервер, на который будет идти проксирование
+            changeOrigin: true,
+        })
+    );
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://217.28.222.68:6050', // Сервер, на который будет идти проксирование
+            changeOrigin: true,
+        })
+    );
+    app.use(
+        '/ws',
+        createProxyMiddleware({
+            target: 'http://217.28.222.68:6101',  // Адрес WebSocket сервера
+            ws: true,                         // Включение поддержки WebSocket
+            changeOrigin: true,
+        })
+    );
+};
+
+
+// "proxy": "http://217.28.222.68:6050"
