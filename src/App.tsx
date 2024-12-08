b09e49054b5c9598d23371364f60e178407ab8f6
asepopov <asepopov@avito.ru>
2024-12-08 23:06:56 +0300

    add possible to display another peer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import MainPage from "./pages/Main";
 import ProfilePage from "./pages/Profile";
 import EditProfilePage from "./pages/EditProfilePage";
 import SocietyAddingPage from './pages/SocietyAddingPage';
+import PeerPage from "./components/PeerPage/PeerPage";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Route path="/profile" element={<ProfilePage/>}/>
             <Route path="/edit" element={<EditProfilePage/>}/>
             <Route path="/new-society" element={<SocietyAddingPage />} />
+            <Route path="/peer/:uuid" element={<PeerPage/>} />
 
             {/*/!* Fallback для всех несуществующих роутов *!/*/}
             {/*<Route path="*" element={<div>Not Found Page</div>}/>*/}
